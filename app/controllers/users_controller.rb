5eddddcce8ad7ad9e64a90471f544715e1f47438
Sunny Hundal <amandip.h@gmail.com>
2023-11-06 13:21:38 -0800

    Added comments for user auth feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def new
   end
-
+# If the user saves, set the session user_id to the new user's id and redirect them to the root path, otherwise redirect them to the signup path.
   def create
     user = User.new(user_params)
     if user.save
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 end
 
 private
-
+#  Define a new method called user_params that returns the list of allowed parameters from the params object.
 def user_params
   params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 end
