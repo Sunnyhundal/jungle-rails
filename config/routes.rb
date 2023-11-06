e0d97edc49ebca7d63043a5ab07378387655be6b
Sunny Hundal <amandip.h@gmail.com>
2023-11-06 10:39:03 -0800

    added routes for user registration and login

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,12 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :create, :new]
   end
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
