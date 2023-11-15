10d36e1330b4d9614fde6e6d99c46b9a708d1f56
Sunny Hundal <amandip.h@gmail.com>
2023-11-15 12:19:33 -0800

    Create test to click on a product partial

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,19 @@
+describe("Home page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("land on the home page", () => {
+    cy.url().should("include", "/");
+  });
+  it("there should be products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+  it("Click on the first product", () => {
+    cy.get(".products article:first-child").click();
+  });
+});
